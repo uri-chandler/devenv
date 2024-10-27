4ca2e7f130b648363b8a92d49e21456eaca70ffd
{author_name} <{author_email}>
2024-10-27 09:11:05 +0200

    chore: first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# devenv
+
+My development environment setup scripts across time and space.
+
+## Features
+
+- [x] Color logging
+- [x] Timestamped log files
+- [ ] Auto-delete old log files
+- [ ] Symlinks dot-files
+
+## Oh My ZSH
+
+- [ ] Aliases
+- [ ] Functions
+- [ ] Theme
+- [ ] Plugins
+
+## Shell
+
+- [x] Oh My ZSH
+- [x] Warp Terminal
+- [x] Homebrew
+- [x] FFMPEG
+- [x] JQ
+- [x] Tree
+- [x] Watch
+
+## Cloud
+
+- [x] Minikube
+- [x] Kubernetes CLI
+- [x] Helm
+- [x] K9s
+- [x] Docker
+- [x] Ngrok
+
+## Coding
+
+- [x] XCode Command Line Tools
+- [x] Volta
+- [x] Node.js
+- [x] Anaconda
+- [x] Go
+- [x] Go Task
+- [x] VS Code
+
+## Productivity
+
+- [x] Chrome
+- [x] Raycast
+- [x] 1Password
+- [x] Notion
+- [x] Asana
+
+## Social
+
+- [x] WhatsApp
+- [x] Telegram
+- [x] Slack
---
+++ setup.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+#####################################################################################################
+#                                                                                                   #
+# Logging Helpers                                                                                   #
+#                                                                                                   #
+#####################################################################################################
+
+SCRIPT_NAME="devenv"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+LOG_FILE="setup-${TIMESTAMP}.log"
+touch $LOG_FILE
+
+# Define color codes
+RESET="\033[0m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+RED="\033[1;31m"
+BLUE="\033[1;34m"
+
+# Logging functions
+log() {
+  echo -e "${WHITE}[${CYAN}${SCRIPT_NAME}${WHITE}] $1${RESET}"
+  echo -e "[${SCRIPT_NAME}] $1" | sed 's/\x1b\[[0-9;]*m//g' >> $LOG_FILE
+}
+
+warn() {
+  echo -e "${WHITE}[${CYAN}${SCRIPT_NAME}${WHITE}] ${YELLOW}$1${RESET}"
+  echo -e "[${SCRIPT_NAME}] $1" | sed 's/\x1b\[[0-9;]*m//g' >> $LOG_FILE
+}
+
+debug() {
+  echo -e "${WHITE}[${CYAN}${SCRIPT_NAME}${WHITE}] ${BLUE}$1${RESET}"
+  echo -e "[${SCRIPT_NAME}] $1" | sed 's/\x1b\[[0-9;]*m//g' >> $LOG_FILE
+}
+
+ok() {
+  echo -e "${WHITE}[${CYAN}${SCRIPT_NAME}${WHITE}] ${GREEN}$1${RESET}"
+  echo -e "[${SCRIPT_NAME}] $1" | sed 's/\x1b\[[0-9;]*m//g' >> $LOG_FILE
+}
+
+error() {
+  echo -e "${WHITE}[${CYAN}${SCRIPT_NAME}${WHITE}] ${RED}$1${RESET}"
+  echo -e "[${SCRIPT_NAME}] $1" | sed 's/\x1b\[[0-9;]*m//g' >> $LOG_FILE
+}
+
+# Example usage
+#
+# log "This is a standard log message."
+# warn "This is a warning message."
+# debug "This is a debug message."
+# ok "This task is completed successfully."
+# error "This is an error message."
+
+
+
+
+#####################################################################################################
+#                                                                                                   #
+# Helper Functions                                                                                  #
+#                                                                                                   #
+#####################################################################################################
+
+# Function to run a command and log output to the logfile
+#
+run() {
+    command="$1"
+    eval "$command" >> "$LOG_FILE" 2>&1
+    return $?
+}
+
+
+
+
+
+#####################################################################################################
+#                                                                                                   #
+# Tools To Install                                                                                  #
+#                                                                                                   #
+#####################################################################################################
+
+# Synax: "Name|Check Command|Install Command"
+#
+TOOLS=(
+    # Xcode Command Line Tools
+    "Xcode Command Line Tools|xcode-select -p|xcode-select --install"
+
+    # Homebrew
+    "Homebrew|brew --version|/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+
+    # Oh My ZSH
+    "Oh My ZSH|zsh --version|sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
+
+    # Volta
+    "Volta|volta --version|/bin/bash -c \"\$(curl -fsSL https://get.volta.sh)\""
+
+    # Node.js LTS using Volta
+    "Node.js LTS|node --version|volta install node@lts"
+)
+
+
+
+
+
+
+#####################################################################################################
+#                                                                                                   #
+# Starting The Setup                                                                                #
+#                                                                                                   #
+#####################################################################################################
+
+echo ""
+log "Starting"
+
+# Iterate over the list of TOOLS and check/install each one
+#
+for tool in "${TOOLS[@]}"; do
+
+    # Line break for better readability
+    echo "" | tee -a $LOG_FILE
+
+    # Get the tool name, check command and install command
+    IFS='|' read -r name check_cmd install_cmd <<< "$tool"
+    
+    # Log the tool name and check command
+    log "Tool: ${CYAN}$name"
+    warn " -> Checking if installed using: ${WHITE}$check_cmd"
+
+    # Run the check command to see if the tool is already installed
+    if ! run "$check_cmd"; then
+        warn " -> Not found (installing)"
+
+        # Try to install the tool
+        # 
+        if run "$install_cmd"; then
+            ok " -> Installed completed successfully"
+        else
+            error " -> Failed to install $name (see $LOG_FILE for details)"
+        fi
+    else
+        ok " -> Ok (already installed)"
+    fi
+done
+
+
+# Make sure Homebrew is up-to-date
+#
+echo "" | tee -a $LOG_FILE
+log "Updating Homebrew"
+if run "brew update"; then
+    ok " -> Homebrew updated successfully"
+else
+    error " -> Failed to update Homebrew (see $LOG_FILE for details)"
+fi
+
+
+# Install Homebrew apps and tools from Brewfile
+#
+echo "" | tee -a $LOG_FILE
+log "Installing apps and tools from Brewfile (this might take a while)"
+if run "brew bundle --file=~/devenv/Brewfile"; then
+    ok " -> Brewfile installed successfully"
+else
+    error " -> Failed to install Brewfile (see $LOG_FILE for details)"
+fi
